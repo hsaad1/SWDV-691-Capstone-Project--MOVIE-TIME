a9818ba75d879be41fe99bc49c4f8df0f1a97da3
hsaad1 <shreyanshi.jain@predikly.com>
2022-04-08 09:06:38 -0700

    JS files

---
+++ js/init.js
@@ -0,0 +1,6 @@
+const moviesContainer = document.querySelector('#movies-container');
+const moviesSearchable = document.querySelector('#movies-searchable');
+const MOVIE_DB_API = 'd8bf019d0cca372bd804735f172f67e8';
+const MOVIE_DB_ENDPOINT = 'https://api.themoviedb.org';
+const MOVIE_DB_IMAGE_ENDPOINT = 'https://image.tmdb.org/t/p/w500';
+const DEFAULT_POST_IMAGE = 'https://via.placeholder.com/150';
